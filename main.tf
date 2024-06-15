2914e299977924760545f6ffad469dfb96075f59
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-16 00:25:44 +0200

    Add VPC

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ provider "aws" {
   region  = "eu-central-1"
   profile = "easytrain"
 }
+
+locals {
+  cidr-vpc = "10.0.0.0/16"
+  name     = "EasyTrain-"
+  vpc-id   = aws_vpc.easytrain-vpc.id
+}
+
+
+resource "aws_vpc" "easytrain-vpc" {
+  cidr_block           = local.cidr-vpc
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${local.name}vpc"
+  }
+}
