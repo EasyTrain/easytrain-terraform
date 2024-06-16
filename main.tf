3134f7faaf1bc3feb541e9f8737d49df974c315e
jacques-navarro <4421229+jacques-navarro@users.noreply.github.com>
2024-06-16 09:54:02 +0200

    Add security group, security group rules and EC2 instance
    - Ingress rules added: SSH, HTTP and ICMP
    - Egress rules: Allow access from any port. Required to install
      openjdk-21-jdk and postgresql

--- main.tf
+++ main.tf
@@ -5,8 +5,10 @@ provider "aws" {
 
 locals {
   cidr-vpc = "10.0.0.0/16"
+  cidr-all = "0.0.0.0/0"
   name     = "EasyTrain-"
   vpc-id   = aws_vpc.easytrain-vpc.id
+  sg-id    = aws_security_group.easytrain-sg.id
 }
 
 
@@ -59,4 +61,76 @@ resource "aws_route_table" "easytrain-rt" {
 resource "aws_route_table_association" "easytrain-rta" {
   route_table_id = aws_route_table.easytrain-rt.id
   subnet_id      = aws_subnet.easytrain-subnet-pub1.id
+}
+
+resource "aws_security_group" "easytrain-sg" {
+  vpc_id = local.vpc-id
+  name = "${local.name}sg"
+
+  tags = {
+    Name = "${local.name}sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "easytrain-igr-ssh" {
+  security_group_id = local.sg-id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+
+  tags = {
+    Name = "${local.name}igr-ssh"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "easytrain-igr-http" {
+  security_group_id = local.sg-id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+
+  tags = {
+    Name = "${local.name}igr-http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "easytrain-igr-icmp" {
+  security_group_id = local.sg-id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 8
+  ip_protocol       = "icmp"
+  to_port           = 0
+
+  tags = {
+    Name = "${local.name}igr-icmp"
+  }
+}
+
+# Required to install openjdk-21-jdk, postgresql
+resource "aws_vpc_security_group_egress_rule" "easytrain-egr-updates" {
+  security_group_id = local.sg-id
+  cidr_ipv4         = local.cidr-all
+  from_port         = 1
+  ip_protocol       = "tcp"
+  to_port           = 65535
+
+  tags = {
+    Name = "${local.name}egr-updates"
+  }
+}
+
+resource "aws_instance" "easytrain-ec2" {
+  # Ubuntu Server 24.04 LTS
+  ami                         = "ami-01e444924a2233b07"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.easytrain-subnet-pub1.id
+  associate_public_ip_address = true
+  key_name                    = "ssh_aws_easytrain_ed25519"
+  vpc_security_group_ids      = ["${local.sg-id}"]
+
+  tags = {
+    Name = "${local.name}ec2"
+  }
 }
